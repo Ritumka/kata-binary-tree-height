ef536fb80fc0cbfd14720ee8b9929b9c262dcbd3
Ian Heraty <heratyian@gmail.com>
2025-04-11 11:17:39 -0500

    Add more tests

--- tests/test_solution.rb
+++ tests/test_solution.rb
@@ -2,6 +2,10 @@ require "minitest/autorun"
 require_relative "../solution"
 
 class TestSolution < Minitest::Test
+  def test_empty_tree
+    assert_equal 0, BinaryTreeNode.height(nil)
+  end
+
   def test_single_node_tree
     root = BinaryTreeNode.new
 
@@ -10,20 +14,39 @@ class TestSolution < Minitest::Test
 
   def test_3_level_balanced_tree
     root = BinaryTreeNode.new(1)
+
     root.left = BinaryTreeNode.new(2)
     root.right = BinaryTreeNode.new(3)
+
     root.left.left = BinaryTreeNode.new(4)
     root.left.right = BinaryTreeNode.new(5)
+    root.right.left = BinaryTreeNode.new(4)
+    root.right.right = BinaryTreeNode.new(5)
 
     assert_equal 3, BinaryTreeNode.height(root)
   end
 
-  def test_3_level_unbalanced_tree
+  def test_left_skewed_tree
     root = BinaryTreeNode.new(1)
+
     root.left = BinaryTreeNode.new(2)
-    root.right = BinaryTreeNode.new(4)
+
     root.left.left = BinaryTreeNode.new(3)
 
-    assert_equal 3, BinaryTreeNode.height(root)
+    root.left.left.left = BinaryTreeNode.new(4)
+
+    assert_equal 4, BinaryTreeNode.height(root)
+  end
+
+  def test_right_skewed_tree
+    root = BinaryTreeNode.new(1)
+
+    root.right = BinaryTreeNode.new(2)
+
+    root.right.right = BinaryTreeNode.new(3)
+
+    root.right.right.right = BinaryTreeNode.new(4)
+
+    assert_equal 4, BinaryTreeNode.height(root)
   end
 end
